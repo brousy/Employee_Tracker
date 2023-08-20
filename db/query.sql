c49812710a848d9bde5ab6defd989135defc699e
Elizabeth Quinn <brousy2222@gmail.com>
2023-08-20 08:57:15 -0400

    sql files populated

---
+++ db/query.sql
@@ -0,0 +1,23 @@
+SELECT
+    employee.id,
+    CONCAT(
+        employee.first_name,
+        ' ',
+        employee.last_name
+    ) AS employee_name,
+
+    role.title,
+    department.name AS department,
+    role.salary,
+
+      CONCAT(
+        manager.first_name,
+        ' ',
+        manager.last_name
+    ) AS manager_name
+
+FROM employee
+LEFT JOIN role ON employee.role_id = role.id
+LEFT JOIN department ON role.department_id = department.id
+-- ask for help to explain the last JOIN
+LEFT JOIN employee AS manager ON employee.manager_id = manager.id;
---
+++ db/seeds.sql
@@ -0,0 +1,30 @@
+-- insert INTO department
+
+INSERT INTO
+-- whatever table I want
+VALUES
+-- determine what those values are
+
+INSERT INTO department (name)
+-- change values of department names
+VALUES ("Lion King"),
+       ("The Godfather"),
+       ("West Side Story"),
+       ("Parasite"),
+       ("The Wizard of Oz");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("CEO", 10000000, 1), 
+        ("Accountant", 100000, 2),
+        ("Sales", 120000, 3), 
+        ("CIO", 250000, 4);
+       
+INSERT INTO employee (
+        first_name,
+        last_name,
+        role_id,
+        manager_id
+    )
+VALUES ("JOHN", "QUINN", 1, NULL),
+-- add people
+        ("BROU", "GRIFFIN", 2, 1);
